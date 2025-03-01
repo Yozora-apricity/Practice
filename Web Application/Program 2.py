1590511e7aab6db1648164e4a5d31a77bb5f4d42
Gerald T. Rogado <geraldtrogado@gmail.com>
2025-03-02 05:50:25 +0800

    hello

---
+++ Web Application/Program 2.py
@@ -0,0 +1,3 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print("Equal" if a == b else "Not equal")
---
+++ Web Application/Program 5.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print ( a / b if b != 0 else "Cannot divide by zero")
